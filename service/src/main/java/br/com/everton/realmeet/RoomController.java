152bd4fb5bbe6adc03724b6959a39bedcfb065f9
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-06-25 17:35:45 -0300

    Aula #2.16 (Prática): Criando uma API com o OpenAPI.

---
+++ service/src/main/java/br/com/everton/realmeet/RoomController.java
@@ -0,0 +1,20 @@
+package br.com.everton.realmeet;
+
+import br.com.everton.realmeet.api.facade.RoomsApi;
+import br.com.everton.realmeet.api.model.Room;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+@RestController
+public class RoomController implements RoomsApi {
+
+    @Override
+    public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
+        return supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
+    }
+}
