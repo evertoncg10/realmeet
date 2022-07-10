8b97c200cef51cf08f6cf2742dffd96b47718dd4
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-07-10 17:53:45 -0300

    Testando git

--- service/src/main/java/br/com/everton/realmeet/RoomController.java
+++ service/src/main/java/br/com/everton/realmeet/RoomController.java
@@ -15,6 +15,7 @@ public class RoomController implements RoomsApi {
 
     @Override
     public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
+		System.print("");
         return supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
     }
 }
